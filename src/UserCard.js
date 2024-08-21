68319dcaa4ab081f51b6a6e8c7977e56d44524d1
Shubham-1-cell <62086183+Shubham-1-cell@users.noreply.github.com>
2024-08-21 14:14:45 +0530

    Push

--- src/UserCard.js
+++ src/UserCard.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
 
-const UserCard = () => {
-
+const UserCard = (props) => {
+    return (
+        <div className='ui card'>
+            
+            <div className='content'>
+                
+                <div className='header'>{props.name}</div>
+                <div className='description'>
+                    {props.children}
+                </div>
+                <div className='ui button bottom'>
+                    <i className='add icon'></i>
+                    Add Friend
+                </div>
+            </div>
+            
+        </div>
+    );
 }
 
 export default UserCard;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Comment from './comment'; // Ensure this path is correct
+import UserCard from './UserCard';
+
+
+//Import Images
 import profile1 from  './media/person1.jpg'
 import profile2 from  './media/person2.jpg'
 import profile3 from  './media/person3.jpg'
@@ -10,10 +14,29 @@ import profile3 from  './media/person3.jpg'
 const App = () => {
   return (
     <div className="ui comments">
-      <h3 className="ui dividing header">Comments</h3>
-      <Comment name='Dave' date='Today at 5:00AM' comment='Hey! Nice work.'pfp={profile1}/>
-      <Comment name='Sarah' date='Yesterday at 4:30PM' comment='wowowowow!' pfp={profile2}/>
-      <Comment name='Clementine' date='3  days ago' comment=':3 <3' pfp={profile3}/>
+        <h3 className="ui dividing header">Comments</h3>
+        <UserCard name='Dave Allen'>      
+            <Comment
+                name='Dave'
+                date='Today at 5:00AM'
+                comment='Hey! Nice work.'
+                pfp={profile1}/>
+        </UserCard>
+        <UserCard name='Sarah Maxwell'>   
+            <Comment 
+                name='Sarah'
+                date='Yesterday at 4:30PM'
+                comment='wowowowow!'
+                pfp={profile2}/>
+            </UserCard>
+        <UserCard name='Clementine Ford'>   
+            <Comment
+                name='Clementine'
+                date='3  days ago'
+                comment=':3 <3'
+                pfp={profile3}/>
+        </UserCard>
+        
     </div>
   );
 };
